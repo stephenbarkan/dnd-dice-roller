49f74af4b8764f67bcbaaf88d6caa2eec72c4e90
Stephen Barkan <stephenbarkan@gmail.com>
2020-05-27 09:49:10 -0500

    bleh

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
           '200': '#FFEDE4',
           '300': '#ffdfd2',
           '500': '#FC664F',
-          '600': '#E8523B',
+          '600': '#e4604c',
         }
       },
 
@@ -29,9 +29,10 @@ module.exports = {
       },
 
       boxShadow: {
-        button: '1px 2px 4px rgba(var(--shadowColor), 0.15), 1px 1px 3px -2px rgba(0, 0, 0, 0.2), inset -2px -3px 11px rgba(var(--shadowColor), 0.15), inset 2px 4px 5px rgba(255, 255, 255, 0.5)',
+        button: '1px 4px 4px rgba(var(--shadowColor), 0.2), 0px 1px 3px rgba(0, 0, 0, 0.25), inset -2px -3px 11px rgba(var(--shadowColor), 0.15), inset 2px 4px 5px rgba(255, 255, 255, 0.8)',
         // buttonHover: 'inset 0 0 15px -3px rgba(var(--shadowColor), .5), inset 0 0 10px -5px rgba(0,0,0, .75), inset 0px 0px 20px -20px rgba(var(--shadowColor),.5)',
-        buttonHover: 'inset 0px 0px 7px -1px rgba(0, 0, 0, .25), inset 4px 4px 7px rgba(255, 255, 255, .5), inset -14px -12px 16px rgba(var(--shadowColor), 0.05), inset 11px 11px 16px rgba(var(--shadowColor), 0.04)',
+        // buttonHover: 'inset 0px 0px 7px -1px rgba(0, 0, 0, .25), inset 4px 4px 7px rgba(255, 255, 255, .5), inset -14px -12px 16px rgba(var(--shadowColor), 0.05), inset 11px 11px 16px rgba(var(--shadowColor), 0.04)',
+        buttonHover: 'inset 0 2px 0 -1px rgba(var(--shadowColor),0.11), inset 0 2px 9px 0 rgba(var(--shadowColor),0.25), inset 0 -1px 0 1px rgba(var(--shadowColor), .125)',
       },
 
       spacing: {
